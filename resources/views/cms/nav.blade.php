23ee61221bc76f2238eb7cbc9801096105643930
Eric Landheer <e.landheer@mentechmedia.nl>
2017-12-08 16:10:42 +0100

    Entity collapsable

--- resources/views/cms/nav.blade.php
+++ resources/views/cms/nav.blade.php
@@ -2,11 +2,16 @@
 <div class="col-lg-12 space-inside-sm">
     
     <!-- header -->
-    <p class="space-inside-sides-md text-color-main text-semi-bold font-xs space-outside-sm">Superadmin</p>
+    <p class="space-inside-sides-md text-color-main text-semi-bold font-xs space-outside-sm pointer" data-toggle="collapse" data-target="#core-admin">
+        Superadmin  
+        <i class="material-icons pointer text-hover-light transition-fast" style="position: relative; top: 9px; left: 20px;">keyboard_arrow_down</i>
+    </p>
 
-    <nav-link id="entity" icon="description"> Entiteiten </nav-link>
-    <nav-link id="section" icon="description"> Secties </nav-link>
-    <nav-link id="navGroup" icon="apps"> Navigatiegroep </nav-link>    
+    <div id="core-admin" class="collapse">
+        <nav-link id="entity" icon="description"> Entiteiten </nav-link>
+        <nav-link id="section" icon="description"> Secties </nav-link>
+        <nav-link id="navGroup" icon="apps"> Navigatiegroep </nav-link>  
+    </div>  
 </div>
 
 <!-- content divider -->
@@ -16,12 +21,18 @@
 <!-- end of content divider -->
 
  @foreach($navGroups as $navGroup)
-<div class="col-lg-12 space-inside-sm">
+<div class="col-lg-12 space-inside-sm" data-toggle="collapse" data-target="#{{ $navGroup->name }}">
     <!-- header -->
-    <p class="space-inside-sides-md text-color-main text-semi-bold font-xs space-outside-sm">{{ $navGroup->name }}</p>
-    @foreach($navGroup->entities as $entity)
-        <nav-link id="{{ $entity->name }}" icon="{{ $entity->icon }}"> {{ $entity->title }} </nav-link>
-    @endforeach
+    <p class="space-inside-sides-md text-color-main text-semi-bold font-xs space-outside-sm pointer">
+        {{ $navGroup->name }} 
+        <i class="material-icons pointer text-hover-light transition-fast" style="position: relative; top: 9px; left: 20px;">keyboard_arrow_down</i>
+    </p>
+    
+    <div id="{{ $navGroup->name }}" class="collapse">
+        @foreach($navGroup->entities as $entity)
+            <nav-link id="{{ $entity->name }}" icon="{{ $entity->icon }}"> {{ $entity->title }} </nav-link>
+        @endforeach
+    </div>
 </div>
 
 <!-- content divider -->
