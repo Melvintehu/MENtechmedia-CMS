25b1e6b5720762618f248d82bba0c7365d2b45f3
Eric Landheer <e.landheer@mentechmedia.nl>
2017-12-23 15:02:26 +0100

    NewsLetter plugin

---
+++ migrate-project.sh
@@ -0,0 +1,2 @@
+php artisan migrate:fresh
+php artisan migrate --path resources/assets/js/cms/core/plugins/NewsLetter/migrations
---
+++ resources/assets/js/cms/core/plugins/NewsLetter/components/view-newsletter.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="col-lg-12 space-outside-up-sm">
+        <div>
+            <h2>Bekijk nieuwsbrieven</h2>
+            <p class="space-inside-sm text-color-accent">
+                Bekijk hieronder de verstuurde nieuwsbrieven.
+            </p>
+
+            <read type="mail"> 
+                   
+            </read>
+        </div>
+   </div>
+</template>
+
+<script>
+   export default {
+       props: {
+
+       },
+
+       data() {
+           return {
+
+           }
+       },
+
+       mounted() {
+
+       },
+
+       methods: {
+          
+       }
+   }   
+</script>
+
